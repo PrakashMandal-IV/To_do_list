0845ec65b25744ed899afaf99896d1af6e0143f5
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-05-18 00:10:51 +0530

    Add : update http request

---
+++ To_do_list/Controllers/ListController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using To_do_list.Data.Service;
+using To_do_list.Data.ViewModel;
+
+namespace To_do_list.Controllers
+{
+    [ApiController]
+    [Route("Controller")]
+    [Authorize]
+    public class ListController : Controller
+    {
+        private ListService _list;
+        public ListController(ListService list)
+        {
+            _list = list;
+        }
+        [HttpPost("add-task")]
+        public ActionResult AddTask([FromBody] ListVM listVM)
+        {
+            var userName = User.FindFirstValue(ClaimTypes.Name);
+            _list.AddToList(listVM, userName);
+            return Ok();
+        }
+        [HttpPost("update-status/{id}")]
+        public ActionResult UpdateTaskStatus(int id)
+        {
+            _list.UpdateStatus(id);
+            return Ok();
+        }
+
+    }
+}
