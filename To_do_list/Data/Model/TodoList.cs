96c48f942ac65d3caf69b3864aea96e488cebb96
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-05-17 19:26:47 +0530

    Add : To Do List Model table

---
+++ To_do_list/Data/Model/TodoList.cs
@@ -0,0 +1,13 @@
+﻿namespace To_do_list.Data.Model
+{
+    public class TodoList
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string WhatToDo { get; set; } = string.Empty;
+        public DateOnly CreatedAt { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public int UserId { get; set; }
+        public User User { get; set; } = default!;
+    }
+}
