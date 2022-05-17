b5a2a8018ba14d2d26b56d5c1266c42777e4d79b
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-05-17 22:05:32 +0530

    Add : SHA256 hasing algo for save password

--- To_do_list/Data/Service/RegistationService.cs
+++ To_do_list/Data/Service/RegistationService.cs
@@ -1,14 +1,17 @@
 ﻿using To_do_list.Data.Model;
 using To_do_list.Data.ViewModel;
+using To_do_list.HashAlgo;
 
 namespace To_do_list.Data.Service
 {
     public class RegistationService
     {
         private AppDbContext _context;
-        public RegistationService( AppDbContext appDbContext)
+        private Hash _hash;
+        public RegistationService( AppDbContext appDbContext,Hash hash)
         {
             _context = appDbContext;
+            _hash = hash;
         }
         public string RegisterUser(Registration registration)
         {
@@ -18,12 +21,13 @@ namespace To_do_list.Data.Service
             }
             else
             {
+                string pass = _hash.HashPass(registration.Password);
                 var user = new User()
                 {
                     UserName = registration.Username,
                     FirstName = registration.FirstName,
                     LastName = registration.LastName,
-                    Password = registration.Password,
+                    Password = pass,
                 };
                 _context.Users.Add(user);
                  user.CreatedAt = DateTime.Now;
