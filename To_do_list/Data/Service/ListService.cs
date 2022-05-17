a9a332a0e22adc659348971e7fd5f44645afe1a8
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-05-18 00:10:27 +0530

    Add : update status method

--- To_do_list/Data/Service/ListService.cs
+++ To_do_list/Data/Service/ListService.cs
@@ -10,8 +10,8 @@ namespace To_do_list.Data.Service
         {
             _context = context;
         }
-
-        public void AddList(ListVM list,string username)
+        //Add task
+        public void AddToList(ListVM list,string username)
         {
             var _user = _context.Users.FirstOrDefault(n => n.UserName == username);
             var _list = new TodoList()
@@ -23,6 +23,21 @@ namespace To_do_list.Data.Service
             _list.UserId = _user?.Id;
             _list.CreatedAt = DateTime.Now;
             _list.IsCompleted = false;
+            _context.SaveChanges();
+        }
+
+        //Update status
+        public void UpdateStatus(int id)
+        {
+            var _task = _context.TodoList.FirstOrDefault(n => n.Id == id);
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            if (!_task.IsCompleted)
+            {
+                _task.IsCompleted = true;
+                
+            }
+            else _task.IsCompleted = false;
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
             _context.SaveChanges();
         }
     }
