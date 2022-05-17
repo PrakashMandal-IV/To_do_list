79c82e1b01e749e401e53a4c8f7e7cdd6017f5a3
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-05-17 21:58:33 +0530

    Add : Registration method

--- To_do_list/Data/Service/RegistationService.cs
+++ To_do_list/Data/Service/RegistationService.cs
@@ -1,4 +1,7 @@
-﻿namespace To_do_list.Data.Service
+﻿using To_do_list.Data.Model;
+using To_do_list.Data.ViewModel;
+
+namespace To_do_list.Data.Service
 {
     public class RegistationService
     {
@@ -6,6 +9,28 @@
         public RegistationService( AppDbContext appDbContext)
         {
             _context = appDbContext;
+        }
+        public string RegisterUser(Registration registration)
+        {
+            if(_context.Users.FirstOrDefault(n =>n.UserName == registration.Username)!= null)
+            {
+                return "Username Already Exist";
+            }
+            else
+            {
+                var user = new User()
+                {
+                    UserName = registration.Username,
+                    FirstName = registration.FirstName,
+                    LastName = registration.LastName,
+                    Password = registration.Password,
+                };
+                _context.Users.Add(user);
+                 user.CreatedAt = DateTime.Now;
+                _context.SaveChanges();
+                return "Successfully Created";
+            }
+            
         }
     }
 }
