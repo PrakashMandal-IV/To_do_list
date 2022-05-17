518f0a09d05e09c6c6dad78f1dc249b8b2b2bdbc
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-05-17 19:30:50 +0530

    Add : Db Context file

---
+++ To_do_list/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace To_do_list.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+    }
+}
