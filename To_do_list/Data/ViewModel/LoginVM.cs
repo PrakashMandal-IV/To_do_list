c51dbb44462c2dee175b312926ae8780b4b6909f
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-05-17 22:31:42 +0530

    Add : Login view model

---
+++ To_do_list/Data/ViewModel/LoginVM.cs
@@ -0,0 +1,8 @@
+﻿namespace To_do_list.Data.ViewModel
+{
+    public class LoginVM
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
