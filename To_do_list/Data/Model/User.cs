c3c0b0dd7c67a08b0e77fa26f3e3ee7b9556e1ca
Prakash Mandal <prakashmandal.iv@gmail.com>
2022-05-17 18:23:47 +0530

    Add : user model

---
+++ To_do_list/Data/Model/User.cs
@@ -0,0 +1,12 @@
+﻿namespace To_do_list.Data.Model
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        public string UserName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateOnly CreatedAt { get; set; }       
+    }
+}
